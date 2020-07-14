59643bd52a7588b54c43ee4df83da47a92fa1ee6
Yakov Rakhamimov <jacobra19@gmail.com>
2020-07-14 23:14:31 +0300

    cmps naming and formating

--- src/components/bio.js
+++ src/components/bio.js
@@ -12,7 +12,7 @@ import Image from "gatsby-image"
 import { rhythm } from "../utils/typography"
 
 const Bio = () => {
-  const data = useStaticQuery(graphql`
+    const data = useStaticQuery(graphql`
     query BioQuery {
       avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
         childImageSharp {
@@ -35,36 +35,35 @@ const Bio = () => {
     }
   `)
 
-  const { author, social } = data.site.siteMetadata
-  return (
-    <div
-      style={{
-        display: `flex`,
-        marginBottom: rhythm(2.5),
-      }}
-    >
-      <Image
-        fixed={data.avatar.childImageSharp.fixed}
-        alt={author.name}
-        style={{
-          marginRight: rhythm(1 / 2),
-          marginBottom: 0,
-          minWidth: 50,
-          borderRadius: `100%`,
+    const { author, social } = data.site.siteMetadata
+    return (
+        <div style={{
+            display: `flex`,
+            marginBottom: rhythm(2.5),
         }}
-        imgStyle={{
-          borderRadius: `50%`,
-        }}
-      />
-      <p>
-        Written by <strong>{author.name}</strong> {author.summary}
-        {` `}
-        <a href={`https://twitter.com/${social.twitter}`}>
-          You should follow him on Twitter
-        </a>
-      </p>
-    </div>
-  )
+        >
+            <Image
+                fixed={data.avatar.childImageSharp.fixed}
+                alt={author.name}
+                style={{
+                    marginRight: rhythm(1 / 2),
+                    marginBottom: 0,
+                    minWidth: 50,
+                    borderRadius: `100%`,
+                }}
+                imgStyle={{
+                    borderRadius: `50%`,
+                }}
+            />
+            <p>
+                Written by <strong>{author.name}</strong> - {author.summary}
+                {` `}
+                <a href={`https://twitter.com/${social.twitter}`}>
+                    You should follow him on Twitter
+                </a>
+            </p>
+        </div>
+    )
 }
 
 export default Bio
