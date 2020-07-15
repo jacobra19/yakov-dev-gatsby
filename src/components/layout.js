964ea02922803ec276750a35cf0cbfbdf98044ef
Yakov Rakhamimov <jacobra19@gmail.com>
2020-07-15 12:02:19 +0300

    header import

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,8 @@
 import React from "react"
+import Header from './header'
 
 import { rhythm } from "../utils/typography"
 
-import Header from './header'
 
 const Layout = ({ location, title, children }) => {
 
