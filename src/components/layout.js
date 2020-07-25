68f99d7ffa3cd037bdbf1e7d5aabd2bb5979f269
Yakov Rakhamimov <jacobra19@gmail.com>
2020-07-25 23:41:16 +0300

    header.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import React from "react"
-import Header from "./header";
-import Footer from "./footer";
+import Header from "./header.js";
+import Footer from "./footer.js";
 
 import { rhythm } from "../utils/typography"
 
